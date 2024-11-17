fd4328c97b5a186a36c1a11e3259922f4981f7ad
vivliwanag <n01697678@humber.ca>
2024-11-17 18:09:12 -0500

    Add files via upload

--- action/action_page.php
+++ action/action_page.php
@@ -37,7 +37,7 @@
     <p align="left">
       <a href="https://linkedin.com/in/vivian-liwanag-a48592261" target="blank"><img align="center" src="https://raw.githubusercontent.com/rahuldkjain/github-profile-readme-generator/master/src/images/icons/Social/linked-in-alt.svg" alt="vivian-liwanag-a48592261" height="30" width="40" /></a>
       <a href="https://instagram.com/vivliwanag" target="blank"><img align="center" src="https://raw.githubusercontent.com/rahuldkjain/github-profile-readme-generator/master/src/images/icons/Social/instagram.svg" alt="vivliwanag" height="30" width="40" /></a>
-      <a href="https://open.spotify.com/user/vivianliwanag2002?si=1531bb7379f64ff8" target="_blank"><img align="center" src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/19/Spotify_logo_without_text.svg/768px-Spotify_logo_without_text.svg.png" alt="vivianliwanag2002" height="30" width="40" /></a>
+      <a href="https://open.spotify.com/user/vivianliwanag2002?si=1531bb7379f64ff8" target="_blank"><img align="center" src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/19/Spotify_logo_without_text.svg/768px-Spotify_logo_without_text.svg.png" alt="vivianliwanag2002" height="40" width="40" /></a>
       </p>
 
   <h6>&copy; 2024 vivliwanag.com</h6>
