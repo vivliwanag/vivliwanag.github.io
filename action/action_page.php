93c203e4a3f9abb38833646e61a93bb91f47f260
vivliwanag <n01697678@humber.ca>
2024-11-17 17:46:11 -0500

    Add files via upload

---
+++ action/action_page.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Action destination</title>
+<meta charset="UTF-8">
+<meta name="description" content="Action destination">
+<link href="css/style.css" rel="stylesheet" type="text/css">
+
+</head>
+
+<body>
+  <nav>
+    <ol>
+      <li><a href="./" target="_blank">HOME</a></li>
+      <li><a href="./about" target="_blank">ABOUT</a></li>
+      <li><a href="./contact" target="_blank">CONTACT</a></li>
+      <li><a href="./artwork" target="_blank">ARTWORK</a></li>
+      <li><a href="./photography" target="_blank">PHOTOGRAPHY</a></li>
+      <li><a href="./graphics" target="_blank">GRAPHICS</a></li>
+    </ol>
+</nav>
+
+<header>
+  <h3 id="title"> MESSAGE SENT 
+    <img class="dogs-running"
+
+    src="img/dogs-running.gif" alt="dogs running">
+  </h3>
+</header>
+
+
+  <hr>
+
+  <footer>
+    <p align="left">
+      <a href="https://linkedin.com/in/vivian-liwanag-a48592261" target="blank"><img align="center" src="https://raw.githubusercontent.com/rahuldkjain/github-profile-readme-generator/master/src/images/icons/Social/linked-in-alt.svg" alt="vivian-liwanag-a48592261" height="30" width="40" /></a>
+      <a href="https://instagram.com/vivliwanag" target="blank"><img align="center" src="https://raw.githubusercontent.com/rahuldkjain/github-profile-readme-generator/master/src/images/icons/Social/instagram.svg" alt="vivliwanag" height="30" width="40" /></a>
+      <a href="https://open.spotify.com/user/vivianliwanag2002?si=1531bb7379f64ff8" target="_blank"><img align="center" src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/19/Spotify_logo_without_text.svg/768px-Spotify_logo_without_text.svg.png" alt="vivianliwanag2002" height="30" width="40" /></a>
+      </p>
+
+  <h6>&copy; 2024 vivliwanag.com</h6>
+  </footer>
+
+</body>
+</html>
